267c1ee869f119228e69e4feca47ece16b0b9d7d
nico_masilla <nicomasilla@gmail.com>
2021-09-23 11:25:33 +0200

    init Landing screen

---
+++ src/frontend/screens/Landing/Landing.js
@@ -0,0 +1,21 @@
+import { Component } from "react";
+import '../landing/Landing.css';
+
+class Landing extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+
+    render() {
+        return (
+            <div>
+                LANDING PAGE
+            </div>
+        )
+    }
+}
