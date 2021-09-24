a79b9ae7b4e9917a3b0fde4f47d6b66029042317
giuseppe_galluzzi <galluzzigiuseppe99@gmail.com>
2021-09-24 12:41:27 +0200

    registration page graphic style completed

--- src/frontend/screens/registrationUser/RegistrationUser.js
+++ src/frontend/screens/registrationUser/RegistrationUser.js
@@ -1,16 +1,108 @@
 import React from "react";
 
+//import Button
+import InputBox from "../../../common/components/ui/inputBox/InputBox";
+import Button from "../../../common/components/ui/button/Button";
+
+import { Link } from "react-router-dom";
+import './RegistrationUser.css'
+
 class RegistrationUser extends React.Component {
+    constructor(props) {
+        super(props);
 
-    render() {
-
-        return (
-            <>
-                RegistrationUser
-            </>
-        )
+        this.state = {
+            userName: "",
+            surname: "",
+            email: "",
+            phone: "",
+            password: "",
+            confpsw: "",
+        }
     }
 
+    handleInputChange = (e) => {
+        console.log(e.target.name);
+        this.setState({ 
+            [e.target.name] : e.target.value 
+        });
+    };
+
+    //TEMP
+    handleSignUp = () => {
+        return true
+    }
+
+    render() {
+        return (
+            <>
+                <h1 style={{ textAlign: 'center' }}>Logo Beije</h1>
+                <h2 style={{ textAlign: 'center' }}>Registration</h2>
+                <form className="container">
+                    <div className="form">
+                    <InputBox
+                        placeholder={"Nome"}
+                        name={"userName"}
+                        type={"text"}
+                        value={this.state.name}
+                        callback={this.handleInputChange}
+                        inputClass={"inputBox"}
+                    />
+                    <InputBox
+                        placeholder={"Cognome"}
+                        name={"surname"}
+                        type={"text"}
+                        value={this.state.surname}
+                        callback={this.handleInputChange}
+                        inputClass={"inputBox"}
+                    />
+
+                    <InputBox
+                        placeholder={"Email"}
+                        name={"email"}
+                        type={"email"}
+                        value={this.state.email}
+                        callback={this.handleInputChange}
+                        inputClass={"inputBox"}
+                    />
+
+                    <InputBox
+                        placeholder={"Numero di Telefono"}
+                        name={"phone"}
+                        type={"tel"}
+                        value={this.state.phone}
+                        callback={this.handleInputChange}
+                        inputClass={"inputBox"}
+                    />
+
+                    <InputBox
+                        placeholder={"Password"}
+                        name={"password"}
+                        type={"password"}
+                        value={this.state.password}
+                        callback={this.handleInputChange}
+                        inputClass={"inputBox"}
+                    />
+
+                    <InputBox
+                        placeholder={"Confirm password"}
+                        name={"confpsw"}
+                        type={"password"}
+                        value={this.state.confpsw}
+                        callback={this.handleInputChange}
+                        inputClass={"inputBox"}
+                    />
+                    {/* validazione dati e invio dei dati */}
+                    <Button
+                        text={"SIGN IN"}
+                        className={"buttonBox"}
+                        callback={this.handleSignUp}
+                    />
+                    </div>
+                </form>
+            </>
+        );
+    }
 }
 
 export default RegistrationUser;
