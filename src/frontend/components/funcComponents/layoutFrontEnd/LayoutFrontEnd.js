bbc42f6fd858d44124919ae125a35868cb01f346
enrico_paolazzi <paolazzienrico@gmail.com>
2021-09-24 15:22:07 +0200

    UPDATE: other fixes on general layouts

--- src/frontend/components/funcComponents/layoutFrontEnd/LayoutFrontEnd.js
+++ src/frontend/components/funcComponents/layoutFrontEnd/LayoutFrontEnd.js
@@ -1,15 +1,33 @@
 import './LayoutFrontEnd.css'
 
-import Navbar from '../../../../backoffice/components/ui/navbar/Navbar';
+import { BrowserRouter as Switch, Route } from "react-router-dom";
+
+import Landing from "../../../screens/landing/Landing";
+import LoginUser from "../../../screens/loginUser/LoginUser";
+import RegistrationUser from "../../../screens/registrationUser/RegistrationUser";
+import ForgotPassword from "../../../screens/forgotPassword/ForgotPassword";
+import UserHome from "../../../screens/userHome/UserHome";
+import Restaurants from "../../../screens/restaurants/Restaurants";
+
+
+import Navbar from '../../ui/navbar/Navbar';
 
 const LayoutFrontEnd = (props) => {
-    return(
+    return (
         <>
-            <div>test</div>
-            <Navbar/>
+            <div>Parte statica Frontend</div>
+            <Navbar />
 
-            {/* props.children carica il contenuto dinamico de */}
-            <div>{props.children}</div>
+            <Switch>
+                <Route path="/" exact component={Landing} />
+                <Route path="/loginUser"  exact component={LoginUser} />
+                <Route path="/registrationUser"  exact component={RegistrationUser} />
+                <Route path="/forgotPassword"  exact component={ForgotPassword} />
+                <Route path="/userHome"  exact component={UserHome} />
+                <Route path="/restaurants"  exact component={Restaurants} />
+            </Switch>
+
+
         </>
     );
 }
--- src/route.js
+++ src/route.js
@@ -1,35 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-
 // Front End Screens an Layout
 import LayoutFrontEnd from "./frontend/components/funcComponents/layoutFrontEnd/LayoutFrontEnd";
 
 // Back Office Screens and Layout
 import LayoutBackOffice from './backoffice/components/funcComponents/layoutBackOffice/LayoutBackOffice';
 
-import { Login as LoginBackOffice } from './backoffice/screens/login/Login';
-
 const Routing = () => {
     return (
-        <Router>
-
-            {/* Front End Routes */}
-
+        <Router>          
             <Switch>
-                <Route exact path="/" component={LayoutFrontEnd} />
-                <Route path="/restaurant/login" component={LoginBackOffice} />
+                <Route path="/" component={LayoutFrontEnd}/>
+                <Route path="/restaurant" component={LayoutBackOffice}/>                       
             </Switch>
-
-
-
-
-
-            {/* Back Office Routes */}
-
-
-
-
         </Router>
     )
 }
