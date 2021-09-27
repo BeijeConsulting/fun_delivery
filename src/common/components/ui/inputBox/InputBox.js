45c1eba3b97610de9df7be21b21931d2784f9555
nico_masilla <nicomasilla@gmail.com>
2021-09-27 15:23:57 +0200

    FIX: changed props name for the onBlur callback

--- src/common/components/ui/inputBox/InputBox.js
+++ src/common/components/ui/inputBox/InputBox.js
@@ -7,8 +7,8 @@ const InputBox = (props) => {
     };
 
     const handleCallbackOnBlur = (e) => {
-        props.callback(e);
-    }
+        props.callbackOnBlur(e);
+    };
 
     return (
         <input
