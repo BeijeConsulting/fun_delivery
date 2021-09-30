f0644d0b2fe86362eaf4c90867513c56bb8a50ae
calogero_messina <c.messina97engineer@gmail.com>
2021-09-30 15:27:42 +0200

    UPDATE: Style screen myMenu ADD: images

--- src/backoffice/screens/myMenu/MyMenu.js
+++ src/backoffice/screens/myMenu/MyMenu.js
@@ -5,7 +5,16 @@ import Card from "../../components/funcComponents/card/Card"
 import 'antd/dist/antd.css';
 // images
 import LogoBeije from '../../assets/images/logo_beijeRosa.png';
-import Primi from '../../assets/images/italiano.jpg'
+import Primi from '../../assets/images/primi.png'
+import Secondi from '../../assets/images/secondi.jfif'
+import Contorni from '../../assets/images/contorni.jpg'
+import Dessert from '../../assets/images/dessert.png'
+import Panini from '../../assets/images/hamburger.jpg'
+import Pizze from '../../assets/images/pizza2.png'
+import Piedine from '../../assets/images/messicano.jpg'
+import Poke from '../../assets/images/poke.jpg'
+import Sushi from '../../assets/images/sushi.png'
+import Altro from '../../assets/images/altro.jpg'
 class MyMenu extends Component {
     constructor(props) {
         super(props)
@@ -30,36 +39,64 @@ class MyMenu extends Component {
 
                             <div className="bo-mymenu-flex-cards">
                                 <Card
-                                    title='Categoria 1'
+                                    title='Primi'
                                     img={Primi}
                                 />
                             </div>
                             <div className="bo-mymenu-flex-cards">
                                 <Card
-                                    title='Categoria 2'
+                                    title='Secondi'
+                                    img={Secondi}
                                 />
                             </div>
                             <div className="bo-mymenu-flex-cards">
                                 <Card
-                                    title='Categoria 3'
+                                    title='Contorni'
+                                    img={Contorni}
                                 />
                             </div>
                             <div className="bo-mymenu-flex-cards">
                                 <Card
-                                    title='Categoria 4' />
-                            </div>
-                            <div className="bo-mymenu-flex-cards">
-                                <Card
-                                    title='Categoria 5'
+                                    title='Dessert'
+                                    img={Dessert}
                                 />
                             </div>
                             <div className="bo-mymenu-flex-cards">
                                 <Card
-                                    title='Categoria 6' />
+                                    title='Panini'
+                                    img={Panini}
+                                />
                             </div>
                             <div className="bo-mymenu-flex-cards">
                                 <Card
-                                    title='Categoria 7' />
+                                    title='Pizze'
+                                    img={Pizze}
+                                />
+                            </div>
+                            <div className="bo-mymenu-flex-cards">
+                                <Card
+                                    title='Messicano'
+                                    img={Piedine}
+                                />
+
+                            </div>
+                            <div className="bo-mymenu-flex-cards">
+                                <Card
+                                    title='Poke'
+                                    img={Poke}
+                                />
+                            </div>
+                            <div className="bo-mymenu-flex-cards">
+                                <Card
+                                    title='Sushi'
+                                    img={Sushi}
+                                />
+                            </div>
+                            <div className="bo-mymenu-flex-cards">
+                                <Card
+                                    title='Altro'
+                                    img={Altro}
+                                />
                             </div>
                         </div>
                     </div>
