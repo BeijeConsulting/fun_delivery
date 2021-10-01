9fbc1cc14157032cd4fd2adcda3d4445a0adb449
enrico_paolazzi <paolazzienrico@gmail.com>
2021-10-01 11:41:41 +0200

    UPDATE: Added class 'active' to current location in sidebar

--- src/backoffice/components/funcComponents/layoutBackOffice/LayoutBackOffice.js
+++ src/backoffice/components/funcComponents/layoutBackOffice/LayoutBackOffice.js
@@ -5,10 +5,14 @@ import Button from '../../../../common/components/ui/button/Button';
 import { Layout } from 'antd';
 import Navbar from '../../ui/navbar/Navbar';
 import { UserOutlined, ShoppingOutlined, ExclamationCircleOutlined, AppstoreOutlined, DollarCircleOutlined } from '@ant-design/icons';
+import { useLocation } from 'react-router-dom'
 
 const { Content, Sider } = Layout;
 
 const LayoutBackOffice = (props) => {
+
+    const pathname = useLocation().pathname;
+
     return (
         <div className="backoffice-layout">
 
@@ -27,11 +31,11 @@ const LayoutBackOffice = (props) => {
                     >
                         <div className="backoffice-menu-container">
                             <ul className="backoffice-menu">
-                                <li><Link to="/restaurant/profile"><span><UserOutlined /></span>Profilo</Link></li>
-                                <li><Link to="/restaurant/my-menu"><span><AppstoreOutlined /></span>Il tuo Menu</Link></li>
-                                <li><Link to="/restaurant/my-orders"><span><ShoppingOutlined /></span>Ordini</Link></li>
-                                <li><Link to="/restaurant/incoming-orders"><span><ExclamationCircleOutlined /></span>In arrivo</Link></li>
-                                <li><Link to="/restaurant/sponsor"><span><DollarCircleOutlined /></span>Sponsor</Link></li>
+                                <li className={pathname === '/restaurant/profile' ? 'active' : ''}><Link to="/restaurant/profile"><span><UserOutlined /></span>Profilo</Link></li>
+                                <li className={pathname === '/restaurant/my-menu' ? 'active' : ''}><Link to="/restaurant/my-menu"><span><AppstoreOutlined /></span>Il tuo Menu</Link></li>
+                                <li className={pathname === '/restaurant/my-orders' ? 'active' : ''}><Link to="/restaurant/my-orders"><span><ShoppingOutlined /></span>Ordini</Link></li>
+                                <li className={pathname === '/restaurant/incoming-orders' ? 'active' : ''}><Link to="/restaurant/incoming-orders"><span><ExclamationCircleOutlined /></span>In arrivo</Link></li>
+                                <li className={pathname === '/restaurant/sponsor' ? 'active' : ''}><Link to="/restaurant/sponsor"><span><DollarCircleOutlined /></span>Sponsor</Link></li>
                             </ul>
 
                             <div className="bo-btn-logout">
