906ea3aea98b73cf1d0d1870e1b68b5e2ab2bbda
enrico_paolazzi <paolazzienrico@gmail.com>
2021-10-04 17:54:53 +0200

    FIX: route path of single plate screen changed

--- src/common/utils/properties.js
+++ src/common/utils/properties.js
@@ -6,7 +6,7 @@ const properties = {
         PROFILE: '/restaurant/profile',
         MY_MENU: '/restaurant/my-menu',
         PLATES: '/restaurant/plates',
-        SINGLE_PLATE: '/restaurant/single-plate',
+        SINGLE_PLATE: '/restaurant/plates/single-plate',
         NEW_PLATE:'/restaurant/new-plate',
         MY_ORDERS: '/restaurant/my-orders',
         SINGLE_ORDER: '/restaurant/my-orders/single-order',
