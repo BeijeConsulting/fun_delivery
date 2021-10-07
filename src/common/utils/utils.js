82694fa798a3dc544b9aee34e29d79ed0c9748aa
lorenzo <lorechiesa57@gmail.com>
2021-10-07 10:15:53 +0200

    FIX: Egle non è più discriminata

--- src/common/utils/utils.js
+++ src/common/utils/utils.js
@@ -3,7 +3,7 @@
 const utils = {
     validateEmail: (params) => { return /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(params) },
     validatePassword: (params) => { return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[_.,\-+*!#@?])([a-zA-Z0-9_.,\-+*!#@?]{6,25})$/i.test(params) },
-    validateName: (params) => { return /^[a-zA-Z]{3,16}$/i.test(params) },
+    validateName: (params) => { return /^([\ A-Za-z])*/.test(params) },
     validateCap: (params) => { return /^\d\d\d\d\d$/i.test(params) },
     validateVAT: (params) => { return /^\d\d\d\d\d\d\d\d\d\d\d$/i.test(params) },
     validatePhone: (params) => { return /^\d\d\d\d\d\d\d\d\d\d$/i.test(params) },
