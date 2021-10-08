fb97d858c8a452d7bd066be6b5d5f3c4e6b12839
christian_agati <christian.agati@gmail.com>
2021-10-08 10:42:15 +0200

    audio on avatar, wheel.

--- src/gamification/screens/memory/Memory.js
+++ src/gamification/screens/memory/Memory.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import './Memory.css'
 import properties from '../../utilities/properties'
 
-/* import musicMemory from "../../assets/sounds/musicMemory.mp3" */
+import musicMemory from "../../assets/sounds/musicMemory.mp3"
 import rightCardsMemory from "../../assets/sounds/rightCardsMemory.mp3"
 import wrongCardsMemory from "../../assets/sounds/wrongCardsMemory.mp3"
 import seeCardMemory from "../../assets/sounds/seeCardsMemory.mp3"
@@ -157,7 +157,7 @@ class Memory extends Component {
 
     render() {
         return (
-            <>
+            <> 
                 <div className='memory-page'>
                     <div className="gm-headerTitleContainer" style={{ height: '80px', border: '3px solid gold' }}>
 
