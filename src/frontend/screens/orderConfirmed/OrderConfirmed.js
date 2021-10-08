0fca1d1a003ed9460b35a2957fc647088ff92faa
mirco_cesaretto <mirco.cesaretto95@gmail.com>
2021-10-08 17:41:37 +0200

    order confirmed modal game

--- src/frontend/screens/orderConfirmed/OrderConfirmed.js
+++ src/frontend/screens/orderConfirmed/OrderConfirmed.js
@@ -2,13 +2,23 @@ import './OrderConfirmed.css'
 import HtmlTag from '../../components/funcComponents/htmlTag/HtmlTag'
 import Button from "../../../common/components/ui/button/Button";
 import gifDelivery from "../../../common/assets/gifDelivery.gif"
+import { useState } from 'react'
+
+import ChooseGame from "../../../gamification/components/funcComponents/chooseGame/ChooseGame"
+import GeneralModal from "../../../gamification/components/funcComponents/generalModal/GeneralModal"
 import { Steps } from 'antd';
 
 const OrderConfirmed = (props) => {
 
+    const [state, setState] = useState({
+        chooseGameModal: false,
+    })
 
     const playWithUs = () => {
-        //al click del button si aprirà la modale di gamification
+
+        setState({
+            chooseGameModal: true,
+        })
     }
 
 
@@ -18,24 +28,22 @@ const OrderConfirmed = (props) => {
 
 
     return (
-        <>
+        <div className="gm-order-confirmed">
             <HtmlTag
                 tag="h1"
                 text="Il tuo ordine sta arrivando!"
                 className="title-order-confirmed" />
 
             <div className="container-gif">
-
-                <img classname="gif-style" src={gifDelivery} alt='delivery guy' />
-
-
+                {!state.chooseGameModal &&
+                    <img className="gif-style" src={gifDelivery} alt='delivery guy' />}
             </div>
 
             <div  >
-                <Steps  current={1} percent={60} direction= "vertical" >
-                    <Step title="In preparazione" description="Il tuo ordine è in preparazione"  />
+                <Steps current={1} percent={60} direction="vertical" >
+                    <Step title="In preparazione" description="Il tuo ordine è in preparazione" />
                     <Step title="Spedito" description="Il rider arriverà a momenti " />
-                    <Step title="Consegnato" description="L'ordine è stato consegnato"  />
+                    <Step title="Consegnato" description="L'ordine è stato consegnato" />
                 </Steps>
             </div>
 
@@ -47,7 +55,13 @@ const OrderConfirmed = (props) => {
                     className={"btn-order-confirmed"}
                     callback={playWithUs} />
             </div>
-        </>
+
+            {
+                state.chooseGameModal &&
+                <GeneralModal
+                    contentModal={<ChooseGame />} />
+            }
+        </div>
 
     )
 }
--- src/gamification/components/funcComponents/chooseGame/ChooseGame.js
+++ src/gamification/components/funcComponents/chooseGame/ChooseGame.js
@@ -3,17 +3,28 @@ import quiz from "../../../assets/images/chooseGame/Quiz.png"
 import memory from "../../../assets/images/chooseGame/memory.png"
 import i18n from "../../../../common/localization/i18n"
 import { withTranslation } from "react-i18next"
-const ChooseGame = () => {
-    const { t }  = this.props;
+import { Link } from "react-router-dom"
+const ChooseGame = (props) => {
+    const { t } = props;
+    const clickQuiz = () => () => {
+        props.history.push("/quiz")
+    }
+    const clickMemory = () => () => {
+        props.history.push("/memory")
+    }
     return (
         <div className="gm-choosegame-container">
             {/* {t('gamification.components.chooseGame.title')} */}
-            <h1>Gioca con noi</h1>
+            <h1 className="gm-choose-title">Gioca con noi</h1>
             {/* {t('gamification.components.chooseGame.subtitle')} */}
             <p className="gm-choosegame-container-p">Gioca con noi in attesa del tuo ordine e guadagna i beijecoins per comprare dei nuovi avatar!</p>
             <div className='gm-choosegame-container-game'>
-                <div className="gm-choosegame-img" ><img src={quiz} alt="quiz" width="100%" /></div>
-                <div className="gm-choosegame-img" ><img src={memory} alt="memory" width="100%" /></div>
+                <Link to="/quiz">
+                    <div onClick={clickQuiz} className="gm-choosegame-img" ><img src={quiz} alt="quiz" width="100%" /></div>
+                </Link>
+                <Link to="/memory">
+                    <div onClick={clickMemory} className="gm-choosegame-img" ><img src={memory} alt="memory" width="100%" /></div>
+                </Link>
             </div>
         </div>
     )
