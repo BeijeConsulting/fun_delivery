609614b3c7694037f70807c8ed5433e7e3789c56
mirco_cesaretto <mirco.cesaretto95@gmail.com>
2021-10-13 14:28:27 +0200

    exp and level in userpage

--- src/frontend/components/ui/userNavbar/UserNavbar.js
+++ src/frontend/components/ui/userNavbar/UserNavbar.js
@@ -3,6 +3,9 @@ import Button from '../../../../common/components/ui/button/Button'
 import { useState } from "react";
 import properties from '../../../../gamification/utilities/properties';
 import '../userNavbar/UserNavbar.css'
+import GeneralModal from "../../../../gamification/components/funcComponents/generalModal/GeneralModal";
+import HeaderModalX from "../../../../gamification/components/funcComponents/headerModalX/HeaderModalX";
+
 
 const UserNavbar = (props) => {
     let levelExp = 1000
@@ -11,7 +14,9 @@ const UserNavbar = (props) => {
     let percentageExp = 0
 
     const [state, setState] = useState({
-        count: 0
+        count: 0,
+        freeDeliveryModal : userPath.freeDeliveryModal
+
     })
 
 
@@ -38,15 +43,26 @@ const UserNavbar = (props) => {
             percentageExp = totalExp / levelExp * 100
             return 4
         }
-        if (userPath.experience >= 10000 && userPath.experience < 16000) {
+        if (userPath.experience >= 10000 && userPath.experience < 15000) {
             totalExp -= 10000
             levelExp = 5000
             percentageExp = totalExp / levelExp * 100
             return 5
-        } if (userPath.experience >= 16000) {
-            totalExp = 5000
-            levelExp = 5000
-            percentageExp = 100
+        } if (userPath.experience >= 15000) {
+            if(userPath.freeDeliveryModal === false){ 
+                setState({
+                    freeDeliveryModal:  userPath.freeDeliveryModal
+                })
+                userPath.freeDeliveryModal = true
+            }else{
+                totalExp = 5000
+                levelExp = 5000
+                percentageExp = 100
+    
+            }
+            userPath.freeDelivery = true
+            localStorage.setItem('userInfo', JSON.stringify(userPath))
+
             return 5
         }
     }
@@ -55,13 +71,23 @@ const UserNavbar = (props) => {
         props.callback(e);
     }
     const addExp = () => {
-        userPath.experience += 1000
-        localStorage.setItem('userInfo', JSON.stringify(userPath))
-        setState({
-            count: state.count + 1
-        })
-        console.log(state.count)
+        if(userPath.experience < 15000){
 
+            userPath.experience += 1000
+            localStorage.setItem('userInfo', JSON.stringify(userPath))
+            setState({
+                count: state.count + 1
+            })
+            console.log(state.count)
+        }
+
+    }
+    const maxLvlModal = () =>{
+        userPath.freeDeliveryModal = true
+            localStorage.setItem('userInfo', JSON.stringify(userPath))
+            setState({
+                freeDeliveryModal : userPath.freeDeliveryModal
+            })
     }
 
     return (
@@ -94,7 +120,10 @@ const UserNavbar = (props) => {
 
                 <span>{userPath.beijeCoin} BC</span>
             </div>
-
+            {!state.freeDeliveryModal && userPath.experience >= 15000 &&
+            <GeneralModal headerModal= {<HeaderModalX callback={maxLvlModal}/>}
+            contentClass="max-level-delivery"
+            contentModal="Hai raggiunto lo status BeijePrime! In omaggio per te spedizioni gratuite a vita!"/>}
         </nav>
     );
 }
