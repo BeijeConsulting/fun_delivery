28ec1a685917a9af1e7e2911c18eff9ea4e47611
nico_masilla <nicomasilla@gmail.com>
2021-10-15 09:29:01 +0200

    NEW: cart implemented

--- src/frontend/components/funcComponents/singlePlate/SinglePlate.js
+++ src/frontend/components/funcComponents/singlePlate/SinglePlate.js
@@ -1,46 +1,36 @@
 import "./SinglePlate.css";
 import { useState } from 'react'
+import Item from "antd/lib/list/Item";
 
 
 const SinglePlate = (props) => {
-
-    const callbackHandler = (quantity) => {
-        let obj =
-        {
-            'name': props.plateName,
-            'price': props.platePrice,
-            'quantity': quantity
-        }  
-
-        return props.callback(obj)
+  
+    const addRemove = (e) =>{
+        return props.callbackHandler(e);
     }
-
-    let quantity = 0;
-
-    const add = () => {
-        quantity ++;
-        callbackHandler(quantity)
-    }
-
-    const remove = () => {
-        if(quantity == 0) return 
-        quantity --;
-        callbackHandler(quantity)
-    }
-
-
+  
     return (
-        <article className={props.classNameWrapper}>
+        <div className={props.classNameWrapper}>
             <header className='textPlate'>
                 <div className='wrapRatingPlate'>
                     <h4 className='titleSinRest'>{props.plateName}</h4>
                     <p className='descriptionPlate'>{props.descriptPlate}</p>
                     <div className='rowButton'>
-                        <div className='pricePlate'> {props.platePrice}</div>
+                        <div className='pricePlate'> {props.platePrice}€</div>
                         <div className='orderSection'>
-                            <button className='buttonAddOrder' value={props.plateName + ' ' + parseInt(props.platePrice) + '€'} onClick={add} >+</button>
-                            {/* <p className='counterOrder'>{counter}</p> */}
-                            {<button className='buttonRemoveOrder' onClick={remove} value={props.plateName + ' ' + parseInt(props.platePrice) + '€'}  >-</button>}
+                            <button 
+                                className='buttonAddOrder' 
+                                value={props.plateName}
+                                operator={'+'} 
+                                onClick={addRemove} >+
+                            </button>
+                            <p className='counterOrder'>{props.quantity}</p>
+                            <button 
+                                className='buttonRemoveOrder' 
+                                operator={'-'} 
+                                onClick={addRemove} 
+                                value={props.plateName} >-
+                            </button>
                         </div>
                     </div>
                 </div>
@@ -49,15 +39,11 @@ const SinglePlate = (props) => {
                     <img className={props.classNameImage} src={props.altImage} alt={props.altText} />
                 </picture>
                 {/* Sarà una prop e ci va la valutazione*/}
-
-
-
             </header>
-        </article>
+        </div>
     )
 }
 export default SinglePlate;
-
 
 
 
