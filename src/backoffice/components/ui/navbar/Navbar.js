df3ca0cff83fe70f34c339359d9e8564b7ba59a0
calogero_messina <c.messina97engineer@gmail.com>
2021-10-15 15:23:16 +0200

    FIX: warning back office

--- src/backoffice/components/ui/navbar/Navbar.js
+++ src/backoffice/components/ui/navbar/Navbar.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import './Navbar.css'
 import Logo from '../../../assets/images/logo_bianco.svg'
-
+import {useHistory} from 'react-router-dom'
 
 const Navbar = (props) => {
+    const history = useHistory()
+    const returnLandingPage = ()=>{
+        history.push('/')
+    }
     return (
 
         <header>
             <div className="bo-navbar">
-                <img src={Logo} alt="" />
+                <img onClick = {returnLandingPage} src={Logo} alt="" style={{cursor:'pointer'}}/>
                 <h2>{props.pageTitle.toUpperCase()}</h2>
             </div>
         </header>
