da62d82a9cea14ceb9e7dcdc7e6cb1716e524cfd
giuseppe_galluzzi <galluzzigiuseppe99@gmail.com>
2021-10-15 11:03:53 +0200

    fixed css landing page

--- src/frontend/screens/landing/Landing.js
+++ src/frontend/screens/landing/Landing.js
@@ -2,29 +2,22 @@ import '../landing/Landing.css';
 import HtmlTag from '../../components/funcComponents/htmlTag/HtmlTag';
 import Input from '../../../common/components/ui/inputBox/InputBox';
 import Button from '../../../common/components/ui/button/Button';
-import Footer from '../../components/funcComponents/footer/Footer'
-
 import burger from '../../../common/assets/HAMBURGER.svg';
 /* import barbecue from '../../../common/assets/BARBECUE.svg'; */
 import iceCream from '../../../common/assets/IceCream.svg';
 import breakFast from '../../../common/assets/Breakfast.svg';
+import backgroundFood from '../../assets/images/background_food.png'
 /* import plateBreakfast from '../../../common/assets/PlateBreakfast.svg'; */
-
-
 //gsap
 import { gsap } from "gsap";
+//AOS 
+import AOS from 'aos';
 /* import { ScrollTrigger } from "gsap/ScrollTrigger"; */
 import { useState } from "react";
 import { useRef } from "react"
 import { useEffect } from "react/cjs/react.development";
-import { useTranslation } from 'react-i18next';
-import { i18n } from 'i18next';
-
-
 const Landing = (props) => {
     const ref = useRef(null);
-    const [t, i18n] = useTranslation()
-
     //STATE
     const [state, setState] = useState({
         addressValue: '',
@@ -34,65 +27,54 @@ const Landing = (props) => {
             left: 10,
         },
     })
-
-
-
-
-
+        
     //USEEFFECT
     useEffect(() => {
         const element = ref.current;
-
         const burgerSel = element.querySelector('.fe-burger-svg')
         const iceCreamSel = element.querySelector('.fe-icecream-svg')
         const breakfastSel = element.querySelector('.fe-breakfast-svg')
-
         gsap.from(burgerSel, { x: 50, opacity: 0, duration: 1, scale: 0.2 })
         gsap.from(iceCreamSel, { y: -50, opacity: 0, duration: 1, scale: 0.2 })
         gsap.from(breakfastSel, { x: -50, opacity: 0, duration: 1, scale: 0.2 })
-
-    }, [])
-
+        //AOS INIT
+        AOS.init({
+                    duration : 1000
+                  })
+            }, [])
     const handleCallbackInputBox = (e) => {
         setState({
             ...state,
             [e.target.name]: e.target.value
         })
     }
-
     const handleCallbackBtn = (e) => {
         props.history.push('/restaurants')
     }
-
-
     const mouseMoveFunction = (e) => {
         setState({
             ...state,
-            burgerOnPage: {
-                bottom: e.pageY / 80,
-                left: -e.pageX / 80,
+            burgerOnPage : {
+                bottom : e.pageY / 80,
+                left : -e.pageX / 80,
             },
         })
     }
-
-    const changeLanguages = (e) => {
-        i18n.changeLanguage(e.target.value)
-    }
-
-
     return (
         <div className='landing-screen' ref={ref} onMouseMove={mouseMoveFunction}>
-            <div className='landing-content'>
+        {/* <video src={Video}autoPlay="true"/> */}
+        <img className='foodBack' src={backgroundFood} data-aos="fade-up"/>
+            <div className='landing-content'data-aos="fade-up">
                 {/* NAV  */}
-
                 <HtmlTag
                     tag='h1'
                     className='main-title'
-                    text={t('frontend.screens.landing_page.title')}
-                />
-                <button value="it" onClick={changeLanguages}>IT</button>
-                <button value="en" onClick={changeLanguages}>EN</button>
-                        
+                    text='Fame? '
+                /><HtmlTag
+                tag='h1'
+                className='main-title'
+                text='Si mangia!'
+            />
                 <div className='main-box'>
                     <Input
                         placeholder='via Roma n.173'
@@ -108,23 +90,19 @@ const Landing = (props) => {
                         callback={handleCallbackInputBox}
                         className='landing-input'
                     />
-
                     <Button
-                        text={t('frontend.components.landing_page.footer.button.find')}
+                        text='Trova Ristoranti'
                         callback={handleCallbackBtn}
                         className='landing-btn'
                     />
-
                 </div>
-
                 
+                
+                {/* FOOTER */}
             </div>
-            <img className='fe-burger-svg fe-svg-landing' src={burger} alt="" style={state.burgerOnPage} />
-            <img className='fe-icecream-svg fe-svg-landing' src={iceCream} alt="" style={state.iceCreamOnPage} />
-            {/* <img className='fe-barbecue-svg fe-svg-landing' src={barbecue} alt="" /> */}
-            <img className='fe-breakfast-svg fe-svg-landing' src={breakFast} alt="" />
+            <img className='fe-burger-svg fe-svg-landing' src={burger} alt="" style={state.burgerOnPage} /> 
+            {/* { <img className='fe-icecream-svg fe-svg-landing' src={iceCream} alt="" style={state.iceCreamOnPage}/> /* */}
         </div>
     );
 }
-
 export default Landing;
