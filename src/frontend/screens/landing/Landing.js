e70cce48a225266445453177a618f579da48c0ab
giuseppe_galluzzi <galluzzigiuseppe99@gmail.com>
2021-10-15 12:21:16 +0200

    added footer on landing page

--- src/frontend/screens/landing/Landing.js
+++ src/frontend/screens/landing/Landing.js
@@ -16,8 +16,13 @@ import AOS from 'aos';
 import { useState } from "react";
 import { useRef } from "react"
 import { useEffect } from "react/cjs/react.development";
+import { useTranslation } from 'react-i18next';
+import { i18n } from 'i18next';
+import Footer from '../../components/funcComponents/footer/Footer';
 const Landing = (props) => {
     const ref = useRef(null);
+
+    const [t, i18n] = useTranslation()
     //STATE
     const [state, setState] = useState({
         addressValue: '',
@@ -27,7 +32,7 @@ const Landing = (props) => {
             left: 10,
         },
     })
-        
+
     //USEEFFECT
     useEffect(() => {
         const element = ref.current;
@@ -39,9 +44,9 @@ const Landing = (props) => {
         gsap.from(breakfastSel, { x: -50, opacity: 0, duration: 1, scale: 0.2 })
         //AOS INIT
         AOS.init({
-                    duration : 1000
-                  })
-            }, [])
+            duration: 1000
+        })
+    }, [])
     const handleCallbackInputBox = (e) => {
         setState({
             ...state,
@@ -54,55 +59,65 @@ const Landing = (props) => {
     const mouseMoveFunction = (e) => {
         setState({
             ...state,
-            burgerOnPage : {
-                bottom : e.pageY / 80,
-                left : -e.pageX / 80,
+            burgerOnPage: {
+                bottom: e.pageY / 80,
+                left: -e.pageX / 80,
             },
         })
     }
+
+    const changeLanguages = (e) => {
+        i18n.changeLanguage(e.target.value)
+    }
+
     return (
-        <div className='landing-screen' ref={ref} onMouseMove={mouseMoveFunction}>
-        {/* <video src={Video}autoPlay="true"/> */}
-        <img className='foodBack' src={backgroundFood} data-aos="fade-up"/>
-            <div className='landing-content'data-aos="fade-up">
-                {/* NAV  */}
-                <HtmlTag
-                    tag='h1'
-                    className='main-title'
-                    text='Fame? '
-                /><HtmlTag
-                tag='h1'
-                className='main-title'
-                text='Si mangia!'
-            />
-                <div className='main-box'>
-                    <Input
-                        placeholder='via Roma n.173'
-                        name='addressValue'
-                        type='text'
-                        value={state.addressValue}
-                        callback={handleCallbackInputBox}
-                        className='landing-input'
-                    />
-                    <Input
-                        name='hourValue'
-                        type='time'
-                        callback={handleCallbackInputBox}
-                        className='landing-input'
-                    />
-                    <Button
-                        text='Trova Ristoranti'
-                        callback={handleCallbackBtn}
-                        className='landing-btn'
-                    />
+        <>
+            <div className='landing-screen' ref={ref} onMouseMove={mouseMoveFunction}>
+                {/* <video src={Video}autoPlay="true"/> */}
+                <img className='foodBack' src={backgroundFood} data-aos="fade-up" />
+                <div className='landing-content' data-aos="fade-up">
+                    {/* NAV  */}
+                    
+                    <HtmlTag
+                        tag='h1'
+                        className='main-title'
+                        text={t('frontend.screens.landing_page.title')}
+                     />
+
+                    <div className='main-box'>
+                        <Input
+                            placeholder='via Roma n.173'
+                            name='addressValue'
+                            type='text'
+                            value={state.addressValue}
+                            callback={handleCallbackInputBox}
+                            className='landing-input'
+                        />
+                        <Input
+                            name='hourValue'
+                            type='time'
+                            callback={handleCallbackInputBox}
+                            className='landing-input'
+                        />
+                        <Button
+                            text={t('frontend.components.landing_page.footer.button.find')}
+                            callback={handleCallbackBtn}
+                            className='landing-btn'
+                        />
+                    </div>
+                    <div className="translation-container">
+                        <button value="it" onClick={changeLanguages}>IT</button>
+                        <button value="en" onClick={changeLanguages}>EN</button>
+                    </div>
+
+
+                    {/* FOOTER */}
                 </div>
-                
-                
-                {/* FOOTER */}
+                <img className='fe-burger-svg fe-svg-landing' src={burger} alt="" style={state.burgerOnPage} />
+                {/* { <img className='fe-icecream-svg fe-svg-landing' src={iceCream} alt="" style={state.iceCreamOnPage}/> /* */}
             </div>
-            <img className='fe-burger-svg fe-svg-landing' src={burger} alt="" style={state.burgerOnPage} /> 
-            {/* { <img className='fe-icecream-svg fe-svg-landing' src={iceCream} alt="" style={state.iceCreamOnPage}/> /* */}
-        </div>
+            <Footer />
+        </>
     );
 }
 export default Landing;
