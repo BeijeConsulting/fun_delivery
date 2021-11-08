8f9eafc2cda83b6da03fa4aad9d809b38ebc2dad
simone_micalizzi <simonemicalizzi.job@gmail.com>
2021-11-08 17:24:29 +0100

    implemented genericServices except for refreshToken

--- src/common/utils/genericServices.js
+++ src/common/utils/genericServices.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import properties from './properties';
+import axios from "axios";
+import properties from "./properties";
 
 //Roberto structure, to be adapted
 class genericServices {
@@ -9,118 +9,119 @@ class genericServices {
             baseURL: properties.BASE_URL, // base url del server
             timeout: 1000, //MS
         });
+
+        // Response interceptor for API calls - Waiting for Ivo
+        // this.instance.interceptors.response.use(
+        //     (response) => {
+        //         return response;
+        //     },
+        //     async function (error) {
+        //         console.log("errorInterceptors,", error);
+        //         const originalRequest = error.config;
+        //         if (error.response.status === 403 && !originalRequest._retry) {
+        //             originalRequest._retry = true;
+        //             const access_token = await this.refreshAccessToken();
+        //             axios.defaults.headers.common["Authorization"] =
+        //                 "Bearer " + access_token;
+        //             return axiosApiInstance(originalRequest);
+        //         }
+        //         return Promise.reject(error);
+        //     }
+        // );
     }
 
-    // Response interceptor for API calls
-    interceptorsResponse = () => {
-        this.instance.interceptors.response.use((response) => {
-            return response
-        }, async function (error) {
-            console.log('errorInterceptors,',error);
-            const originalRequest = error.config;
-            if (error.response.status === 403 && !originalRequest._retry) {
-                originalRequest._retry = true;
-                const access_token = await refreshAccessToken();
-                axios.defaults.headers.common['Authorization'] = 'Bearer ' + access_token;
-                return axiosApiInstance(originalRequest);
-            }
-            return Promise.reject(error);
-        });
-    }
+    checkErrorStatus = (error) => {
+        if (error.response.status === "403") {
+            //Fare il refresh token
+            return error.response.data;
+        }
+        if (error.response.status === "401") {
+            //Email o password errati
+            return error.response.data;
+        }
+    };
 
     getLocalAccessToken = () => {
-        const accessToken = localStorage.getItem("accessToken");
-        return accessToken;
-    }
+        const accessToken = JSON.parse(localStorage.getItem("user"));
+        return accessToken.token;
+    };
 
     getLocalRefreshToken = () => {
         const refreshToken = localStorage.getItem("refreshToken");
         return refreshToken;
-    }
+    };
 
     // funzione per modificare header del server
-    getHeaderWithToken = (auth, lang) => {
+    // getHeaderWithToken = (auth, lang) => {
+    getHeaderWithToken = (auth) => {
+
         let headers = {
             Accept: "*/*",
             "Content-type": "application/json; charset=UTF-8",
         };
-        if (!!lang) {
-            headers["AcceptedLang"] = lang;
-        }
+        // if (!!lang) {
+        //     headers["AcceptedLang"] = lang;
+        // }
         if (!!auth) {
-            headers["AuthToken"] = 'Bearer ' + auth;
+            headers["AuthToken"] = "Bearer " + auth;
         }
-        return headers
-    }
-
+        return headers;
+    };
 
     //Chiamata API CRUD
     // GET (READ)
     apiGET = async (path) => {
-       //  this.instance.defaults.headers = this.getHeaderWithToken(this.getLocalAccessToken(), 'it')
+       // this.instance.defaults.headers = this.getHeaderWithToken(this.getLocalAccessToken())
         return await this.instance.get(path)
             .then((response) => {
                 if (response.status === 200) {
-                    return response.data
+                    return response.data;
                 }
-               // this.interceptorsResponse()
+                // this.interceptorsResponse()
             })
             .catch((error) => {
-                return error
-            })
-    }
-
-    apiLOGIN = async(email, password) => {
-        return await this.instance.post("/signin", {
-            params: {
-                email: email,
-                password: password
-            }
-        })
-
-        .then((response) => {
-            if(response.status===200){
-                return response
-            }
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-    }
+                this.checkErrorStatus(error.response.status);
+            });
+    };
 
     // POST (Create)
     apiPOST = async (path, obj) => {
-        this.instance.defaults.headers = this.getHeaderWithToken('Token', 'it')
+        if(path!=="/signin"){
+            let token = this.getLocalAccessToken();
+            this.instance.defaults.headers = this.getHeaderWithToken(token);
+        }
+
         return await this.instance.post(path, obj)
             .then((response) => {
-                return response
+                if (response.status === 200) {
+                    return response.data;
+                }
             })
             .catch((error) => {
-                return error
-            })
-    }
-
+                this.checkErrorStatus(error.response.status);
+            });
+    };
 
     // PUT (UPDATE)
     apiPUT = async (path, obj) => {
         return await this.instance.put(path, obj)
             .then((response) => {
-                return response
+                return response;
             })
             .catch((error) => {
-                return error
-            })
-    }
+                this.checkErrorStatus(error.response.status);
+            });
+    };
 
     // DELETE (DELETE)
     apiDELETE = async (path) => {
         return await this.instance.delete(path)
             .then((response) => {
-                return response
+                return response;
             })
             .catch((error) => {
-                return error
-            })
-    }
+                this.checkErrorStatus(error.response.status);
+            });
+    };
 }
 export default genericServices;
